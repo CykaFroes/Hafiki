fc45a93916375565994128de04c183af0406fc2a
Nik0 <nikowluiz@gmail.com>
2018-08-01 20:59:02 -0300

    Quase pronto

---
+++ header.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hafiki</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--  Fonts and icons  -->
+      <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+  </head>
+  <body><nav class="navbar  fixed-top navbar-expand-lg"  >
+  <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
+          <img src="assets/img/hafiki-logo.png" /> </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+              <li class="dropdown nav-item">
+                <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false">
+                <i class="material-icons">supervised_user_circle</i> Sobre nos
+                </a>
+                <div class="dropdown-menu dropdown-with-icons">
+                    <a href="clientes.php" class="dropdown-item">
+                        <i class="material-icons">how_to_reg</i> Nossos Clientes
+                    </a>
+                    <a href="clientes.php" class="dropdown-item">
+                    <i class="material-icons">my_location</i> Onde estamos
+                    </a>
+                </div>
+              </li>
+              <li class="nav-item">
+                  <a href="#Contato" class="nav-link">
+                  <i class="material-icons">sms</i> Entre em contato
+                  </a>
+              </li>
+              <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/hafikicomunica" target="_blank" data-original-title="Siga a gente no Twitter">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/hafikicom/" target="_blank" data-original-title="Curta a gente no Facebook">
+              <i class="fa fa-facebook-square"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/hafikicomunicacao/" target="_blank" data-original-title="Siga a gente no Instagram">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://pt.linkedin.com/company/hafiki" target="_blank" data-original-title="Trabalhe conosco">
+              <i class="fa fa-linkedin"></i>
+            </a>
+          </li>
+          </ul>
+      </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php $Titulo = "Hafiki";
+include "header.php"; ?>
+
+
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 ml-auto mr-auto">
+        <div class="brand text-center">
+          <h1>Comunicação além da caixa</h1>
+          <h3 class="title text-center">Muito mais do que uma assessoria</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title">Sigam-me os bons!</h2>
+      <p class="h4"><strong>A Hafiki é uma plataforma única de assessoria de imprensa, que alia relacionamento com a mídia nacional, produção de conteúdo de qualidade, marketing e performance digital, garantindo resultado com foco em um mundo que está cada vez mais digital. Garantindo um atendimento personalizado para os clientes e entregando o melhor de uma assessoria de imprensa.</strong></p>
+    </div>
+  </div>
+</div>
+
+<?php include "footer.php"; ?>
