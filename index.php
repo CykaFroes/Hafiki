eacbb21fa1fb7eb9b654f48743fbb440c6d193f1
Nik0 <nikowluiz@gmail.com>
2018-08-01 23:35:23 -0300

    updated

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ include "header.php"; ?>
 <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
   <div class="container">
     <div class="row">
-      <div class="col-md-8 ml-auto mr-auto">
+      <div class="col-md-12 ml-auto mr-auto">
         <div class="brand text-center">
+          <img src="assets/img/hafiki-header.png" class="mx-auto d-block img-fluid" />
           <h1>Comunicação além da caixa</h1>
           <h3 class="title text-center">Muito mais do que uma assessoria</h3>
         </div>
@@ -27,15 +28,37 @@ include "header.php"; ?>
       <div class="card">
           <div class="card-header card-header-text card-header-primary">
             <div class="card-text">
-              <h3 class="card-title">O que fazemos?</h4>
+              <h3 class="card-title">O que fazemos <i class="fa fa-question-circle"></i></h4>
             </div>
           </div>
           <div class="card-body">
-              <p class="h4">Fazemos os maiores serviços de assessoria de imprensa que voce ja viu </p>
+              <p class="h4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et ex eu lacus commodo convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec dignissim cursus odio, dapibus iaculis ante elementum a. Aenean lobortis justo a lectus lobortis, et dignissim elit ultricies. Curabitur est lacus, ornare vitae rhoncus eu, pellentesque in felis. Morbi a neque est. Vestibulum venenatis ullamcorper magna vel condimentum. Pellentesque eleifend magna nec tincidunt consequat. Phasellus neque augue, porttitor et nunc nec, ornare elementum lorem. Integer egestas nisl ac quam dapibus volutpat.</p>
           </div>
       </div>
   </div>
+  <div class="info info-horizontal col-xs-6 col-sm-3 col-md-3 col-lg-3">
+	  <div class="icon icon-red">
+		  <i class="material-icons">group_work</i>
+	  </div>
+	  <div class="description">
+		  <h4 class="info-title">Lorem ipsum</h4>
+		  <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et ex eu lacus commodo convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et ex eu lacus commodo convallis.</p>
+		  <a href="#none">Saiba mais...</a>
+	  </div>
   </div>
+  <div class="info info-horizontal col-xs-6 col-sm-3 col-md-3 col-lg-3">
+	  <div class="icon icon-red">
+    <i class="material-icons">trending_up</i>
+	  </div>
+	  <div class="description">
+		  <h4 class="info-title">Collaborate</h4>
+		  <p>Donec dignissim cursus odio, dapibus iaculis ante elementum a. Aenean lobortis justo a lectus lobortis, et dignissim elit ultricies.  </p>
+		  <a href="#none">Saiba mais...</a>
+	  </div>
+  </div>
+  
+</div>
 </div>
 </div>
 
